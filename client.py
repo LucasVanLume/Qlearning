a81abf735d8629d9d0d9b258dd2995d3023fae1a
Lucas <lucasvanlume@hotmail.com>
2023-03-07 21:36:55 -0300

    Código finalizado e treinado

--- client.py
+++ client.py
@@ -1,52 +1,85 @@
 #Aqui vocês irão colocar seu algoritmo de aprendizado
 import connection as cn
 import random
-import numpy as np
-import time
 
-
-# Conecta-se ao servidor do jogo
-s = cn.connect(2037)
+state = cn.connect(2037)
 
 # Define as constantes para o algoritmo Q-learning
-ALPHA = 0.5
+ALPHA = 0.7
 GAMMA = 0.9
 EPSILON = 0.1
-NUM_EPISODES = 1000
+NUM_EPISODES = 20
 
 # Define as ações possíveis
-actions = ['left', 'right', 'jump']
+actions = ['left', 'jump', 'right']
 
-# Inicializa a Q-table com zeros
-q_table = np.zeros((96, 3))
-
-
-# Inicia o loop de episódios
 for i in range(NUM_EPISODES):
-    # Reseta o ambiente e obtém o estado inicial
-    action = 'jump'
-    state, reward = cn.get_state_reward(s, action)
+    # variavel da proxima ação
+    acao = random.choice(actions)
+    estado, recompensa = cn.get_state_reward(state, acao)
 
-    # Inicia o loop para cada passo do episódio
-    while reward != -1:
+    while recompensa != 300:
+        # calculo da linha do arquivo resultado.txt a ser modificada
+        posicao = int(estado[2:7], 2)*4 + int(estado[7:], 2) 
+
+        # abrindo documento
+        f = open('resultado.txt', 'r')  
+
+        # Armazenando as linhas do arquivo em uma array
+        resultados = f.readlines()
+
+        # armazenando linha selecionada e recebendo valores separadamente
+        linha = resultados[posicao]
+        numeros_string = linha.split(" ")
+        numeros = []
+
+        for x in numeros_string:
+            numeros.append(float(x))
+
         # Escolhe a ação a ser tomada usando a política epsilon-greedy
-        if random.uniform(0, 0.1) < EPSILON:
-            action = random.choice(actions)
-        #else:
-            ###??? Pegar o máximo valor da tabela q_table
-
+        if random.uniform(0, 1) < EPSILON:
+            acao = random.choice(actions)
+        else:
+            # Pegar o máximo valor da tabela q_table
+            valor = numeros.index(max(numeros[0], numeros[1], numeros[2]))
+            acao = actions[valor]
+        
         # Executa a ação e recebe o novo estado e a recompensa
-        new_state, reward = cn.get_state_reward(s, action)
+        new_state, recompensa = cn.get_state_reward(state, acao)
 
-        # Atualiza a Q-table usando a equação de Q-learning
-        #???q_table[state, action] += ALPHA * (reward + GAMMA * np.max(q_table[new_state]) - q_table[state, action])
+        # calculo da nova linha do arquivo resultado.txt
+        nova_posicao = int(new_state[2:7], 2)*4 + int(new_state[7:], 2)
 
-        # Define o novo estado como o estado atual
-        state = new_state
+        # armazenando nova linha selecionada e recebendo valores separadamente
+        nova_linha = resultados[nova_posicao]
+        novo_numeros_string = nova_linha.split(" ")
+        novo_numeros = []
 
-        # Espera um tempo antes de executar o próximo passo do episódio
-        # time.sleep(0.01)
+        for x in novo_numeros_string:
+            novo_numeros.append(float(x)) 
 
-    # Exibe a recompensa obtida no final do episódio
-    print(f"Episódio {i}: Recompensa = {reward}")
+        #OBS.: A ordem das colunas na Q-Table[left, jump, right]
+        # substituindo o valor de acordo com a ação correspodente
+        if acao == "left":
+            novo_valor = ALPHA * (recompensa + GAMMA * max(float(novo_numeros_string[0]), float(novo_numeros_string[1]), float(novo_numeros_string[2])) - float(numeros_string[0]))
+            atual = str(float(novo_valor) + float(numeros_string[0])) + " " + numeros_string[1] + " " + numeros_string[2]
+        elif acao == "jump":
+            novo_valor = ALPHA * (recompensa + GAMMA * max(float(novo_numeros_string[0]), float(novo_numeros_string[1]), float(novo_numeros_string[2])) - float(numeros_string[1]))
+            atual = numeros_string[0] + " " + str(float(novo_valor) + float(numeros_string[1])) + " " + numeros_string[2]
+        elif acao == "right":
+            novo_valor = ALPHA * (recompensa + GAMMA * max(float(novo_numeros_string[0]), float(novo_numeros_string[1]), float(novo_numeros_string[2])) - float(numeros_string[2]))
+            atual = numeros_string[0]+ " " + numeros_string[1] + " " + str(float(novo_valor) + float(numeros_string[2])) + "\n"
+            atual = atual.replace(r"\n", "\n")
 
+        # reescrevendo a linha selecionada
+        resultados[posicao] = atual
+        print(recompensa, " while")
+
+        # reescrevendo documento com a array modificada
+        f = open('resultado.txt', 'w')
+        f.writelines(resultados)
+
+        estado = new_state
+
+# fechando documento 
+f.close()
